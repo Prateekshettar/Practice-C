74d22bba8d873200a5e0571ddfcfdca10a803bdc
Prateek <55922346+Prateekshettar@users.noreply.github.com>
2022-02-27 23:59:57 +0530

    Add files via upload

---
+++ assignment1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=1;i<=8 ;i++)
+    {
+        if(i==3)
+        {
+            printf("I reached first Milestone \n");
+        }
+        if(i==6)
+        {
+            printf("I reached second Milestone\n");
+        }
+    printf("Routine \n");
+    }
+ }
